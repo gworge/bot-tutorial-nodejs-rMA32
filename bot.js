fcd70d52993567309df48aba342640a2788afbc1
gworge <george.ilikethatform@gmail.com>
2017-04-03 15:32:28 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -76,7 +76,7 @@ function respond() {
   else if(request.text && botRegexDL.test(request.text)) {
     this.res.writeHead(200);
     //postMessage("http://www.daddyleagues.com/maddenrating?name=&position=all&team="+request.text.substring(5,8));
-    postMessage("http://daddyleagues.com/mcf/team/"+request.text.substring(5,8)+"/depthchart");
+    postMessage("http://www.daddyleagues.com/TheGRID"+request.text.substring(5,8)+"/depthchart");
     this.res.end();
   } 
   
